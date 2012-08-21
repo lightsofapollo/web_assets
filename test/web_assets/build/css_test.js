919b431d31399e085d34a832cd83b4860a5791f0
James Lal <james@lightsofapollo.com>
2012-08-21 10:15:03 -0700

    initial css image scrapper

--- test/web_assets/build/css_test.js
+++ test/web_assets/build/css_test.js
@@ -33,6 +33,106 @@ suite('build/css', function() {
     assert.equal(subject.build, build);
   });
 
+  suite('#_findAndResolveImages', function() {
+
+    test('http url - no prefix', function() {
+      var domain = 'http://google.com';
+      source = '\n';
+      source += '.foo {\n' + '  background: url(\'one.js\'); \n}\n';
+      source += '.bar {\n' + '  background: url("two.js")' + '\n}\n';
+      source += '.bar {\n  background: url(/foo/three.js)\n}\n';
+
+      var output = subject._processSourceForImages(
+        domain,
+        source
+      );
+
+      assert.equal(output, source);
+
+      var expected = [
+        { from: domain + '/one.js', to: 'one.js' },
+        { from: domain + '/two.js', to: 'two.js' },
+        { from: domain + '/foo/three.js', to: '/foo/three.js' }
+      ];
+
+      assert.deepEqual(
+        subject._imagesToCopy,
+        expected
+      );
+    });
+
+    test('absolute url - no prefix', function() {
+      var domain = '/Users';
+      source = '\n';
+      source += '.foo {\n' + '  background: url(\'one.js\'); \n}\n';
+      source += '.bar {\n' + '  background: url("two.js")' + '\n}\n';
+      source += '.bar {\n  background: url(/foo/three.js)\n}\n';
+
+      var output = subject._processSourceForImages(
+        domain,
+        source
+      );
+
+      var expected = [
+        { from: domain + '/one.js', to: 'one.js' },
+        { from: domain + '/two.js', to: 'two.js' },
+        { from: '/foo/three.js', to: '/foo/three.js' }
+      ];
+
+      assert.deepEqual(
+        subject._imagesToCopy,
+        expected
+      );
+    });
+
+    test('prefix http', function() {
+      var domain = 'http://cool.com/';
+      var input = 'url(foo.js)';
+      var out = subject._processSourceForImages(
+        domain,
+        input,
+        'cool'
+      );
+
+      assert.equal(
+        out,
+        'url("cool/foo.js")'
+      );
+
+      assert.deepEqual(
+        subject._imagesToCopy,
+        [
+          { from: domain + '/foo.js', to: 'cool/foo.js' }
+        ]
+      );
+    });
+
+    test('prefix local', function() {
+      var domain = '/root';
+      var input = 'url(./foo.js)';
+      var out = subject._processSourceForImages(
+        domain,
+        input,
+        'cool'
+      );
+
+      assert.equal(
+        out,
+        'url("cool/foo.js")'
+      );
+
+      assert.deepEqual(
+        subject._imagesToCopy,
+        [
+          { from: domain + '/foo.js', to: 'cool/foo.js' }
+        ]
+      );
+    });
+
+    test('integration', function() {
+    });
+  });
+
   suite('#process', function() {
     suite('simple concat', function() {
       testSupport.verifyBasicProcess(function() {
